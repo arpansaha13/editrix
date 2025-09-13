962ed7861419453ee748e686dbd435c7b7fa2200
arpansaha13 <arpansaha1300@gmail.com>
2025-09-13 19:26:30 +0530

    feat: show editor in playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,25 +1,11 @@
+import { Editrix } from '@editrix/core'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter.ts'
-
-const viteLogoSrc = '/vite.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogoSrc}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+  <div class="container">
+    <h1>Editrix Demo</h1>
+    <div id="editor"></div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const editor = new Editrix('#editor')
