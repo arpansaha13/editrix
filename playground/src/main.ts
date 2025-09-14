a687e58e9a49d68319086aa201c1f8a9f118a930
arpansaha13 <arpansaha1300@gmail.com>
2025-09-15 00:25:07 +0530

    refactor: update playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,10 +2,17 @@ import { Editrix } from '@editrix/core'
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>Editrix Demo</h1>
   <div class="container">
-    <h1>Editrix Demo</h1>
+    <div>
+      <button id="bold">Bold</button>
+    </div>
     <div id="editor"></div>
   </div>
 `
 
 const editor = new Editrix('#editor')
+
+document
+  .querySelector<HTMLButtonElement>('button#bold')!
+  .addEventListener('click', editor.applyBold)
